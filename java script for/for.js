74661fc5b5c0b3c634a64875ce2839c5409cf364
Brenopiros <101352256+Brenopiros@users.noreply.github.com>
2024-11-22 09:04:08 -0300

    Add files via upload

---
+++ java script for/for.js
@@ -0,0 +1,107 @@
+// const regulararray = [2,5,2,7,1,3,4,7];
+
+// let count = 0;
+// while (count < regulararray.length) {
+//   regulararray[count] += count;
+//   count++;
+  
+// }
+
+// console.log(regulararray);
+
+
+// const regulararray = [2,5,2,7,1,3,4,7];
+// for (let count = 0; count < regulararray.lengtht; count++) {
+//   regulararray[count] += count;
+  
+// }
+// console.log(regulararray);
+
+// const frutas = ['maçã', 'banana','laranja','uva'];
+// for (let i = 0; i < frutas.length; i++) {
+//   console.log(frutas[i]);
+  
+// }
+
+
+// let soma = 0;
+// for (let i = 1; i <= 10; i++) {
+//   soma += i;
+  
+// }
+// console.log(soma);
+
+
+// const pessoa = {
+//   nome: 'breno',
+//   idade: 25,
+//   profissao: 'desenvolvedor'
+// };
+
+// for (let propriedade in pessoa){
+//   console.log(`${propriedade}: ${pessoa[propriedade]}`);
+  
+// }
+
+// const prompt = require('prompt-sync')()
+// const numiteracoes = parseInt(prompt("quantas vezes você quer inserir um número?"))
+// let somatotal = 0;
+
+// for(let i = 1; i<= numiteracoes; i++){
+//   const numero = parseFloat(prompt(`Digite o número ${i}:`))
+//   somatotal += numiteracoes;
+// }
+// console.log(`A soma total dos números inseridos é: ${somatotal}`);
+
+
+// const alunos = ['breno', 'caio', 'samu', 'beatriz', 'guilherme']
+// for (let i = 0; i < alunos.length; i++) {
+//   console.log(alunos[i]);
+  
+// }
+
+
+// let numeros = 5;
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`${numeros} x ${i} = ${numeros*i}`);
+  
+// }
+
+// let nota = [10.0,8.0,7.0,9.0,10.0];
+// let total = 0;
+// for (let i = 0; i < nota.length; i++) {
+//   total += nota[1];
+  
+//   }
+//   let media = total / nota.length;
+//   console.log(`Sua media é ${media}`);
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+  
+// }
+
+
+// let pares = 0;
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 ===0)
+//     console.log(i);
+    
+
+// }
+
+
+// let materiais = ['caderno', 'lapis', 'caneta', 'livro didatico',]
+// for (let i = 0; i < materiais.length; i++) {
+//   console.log(  materiais[i]);
+  
+// }
+
+// let n = 5;
+// let soma = 0;
+// for (let i = 1; i <= n; i++) {
+//   soma += i;
+  
+// } 
+// console.log(soma);
